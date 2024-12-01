513671081923ca98c61ecd2bcafac0ff829c04a3
Aymane BOUMAAZA <aymane.boumaaza@innovx.ma>
2024-12-01 12:16:27 +0100

    adapt tests

--- exercise/python/day01/tests/test_SantaCommunicator.py
+++ exercise/python/day01/tests/test_SantaCommunicator.py
@@ -2,6 +2,7 @@ import unittest
 
 from assertpy import assert_that
 
+from communication.reindeer import Reindeer
 from communication.santa_communicator import SantaCommunicator
 from tests.doubles.logger import TestLogger
 
@@ -14,24 +15,30 @@ class SantaCommunicatorTest(unittest.TestCase):
         self.communicator = SantaCommunicator(self.number_of_days_to_rest)
 
     def test_compose_message(self):
-        message = self.communicator.compose_message("Dasher", "North Pole", 5, self.number_of_day_before_christmas)
+        message = self.communicator.compose_message(
+            Reindeer("Dasher", "North Pole", 5),
+            self.number_of_day_before_christmas,
+        )
         assert_that(message).is_equal_to(
             "Dear Dasher, please return from North Pole in 17 day(s) to be ready and rest before Christmas."
         )
 
     def test_is_overdue_detect_overdue_reindeer(self):
         overdue = self.communicator.is_overdue(
-            "Dasher", "North Pole", self.number_of_day_before_christmas,
-            self.number_of_day_before_christmas, self.logger
+            Reindeer("Dasher", "North Pole", self.number_of_day_before_christmas),
+            self.number_of_day_before_christmas,
+            self.logger,
         )
         assert_that(overdue).is_true()
-        assert_that(self.logger.get_log()).is_equal_to("Overdue for Dasher located North Pole.")
+        assert_that(self.logger.get_log()).is_equal_to(
+            "Overdue for Dasher located North Pole."
+        )
 
     def test_is_overdue_return_false_when_not_overdue(self):
         overdue = self.communicator.is_overdue(
-            "Dasher", "North Pole",
+            Reindeer("Dasher", "North Pole", 5),
             self.number_of_day_before_christmas - self.number_of_days_to_rest - 1,
-            self.number_of_day_before_christmas, self.logger
+            self.logger,
         )
         assert_that(overdue).is_false()
 
