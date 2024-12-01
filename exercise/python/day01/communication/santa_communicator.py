dcefb7bb7288a6538a8823526a53782e8ef455ed
Aymane BOUMAAZA <aymane.boumaaza@innovx.ma>
2024-12-01 12:16:20 +0100

    refactor santa_communicator and use reindeer class

--- exercise/python/day01/communication/santa_communicator.py
+++ exercise/python/day01/communication/santa_communicator.py
@@ -1,34 +1,36 @@
+from communication.reindeer import Reindeer
+
+
 class SantaCommunicator:
     def __init__(self, number_of_days_to_rest: int) -> None:
         self.number_of_days_to_rest = number_of_days_to_rest
 
     def compose_message(
         self,
-        reindeer_name: str,
-        current_location: str,
-        numbers_of_days_for_coming_back: int,
+        reindeer: Reindeer,
         number_of_days_before_christmas: int,
     ) -> str:
         days_before_return = self._days_before_return(
-            numbers_of_days_for_coming_back, number_of_days_before_christmas
+            reindeer.number_of_days_for_coming_back, number_of_days_before_christmas
         )
-        return f"Dear {reindeer_name}, please return from {current_location} in {days_before_return} day(s) to be ready and rest before Christmas."
+        return f"Dear {reindeer.name}, please return from {reindeer.current_location} in {days_before_return} day(s) to be ready and rest before Christmas."
 
     def is_overdue(
         self,
-        reindeer_name: str,
-        current_location: str,
-        numbers_of_days_for_coming_back: int,
+        reindeer: Reindeer,
         number_of_days_before_christmas: int,
         logger,
     ) -> bool:
         if (
             self._days_before_return(
-                numbers_of_days_for_coming_back, number_of_days_before_christmas
+                reindeer.number_of_days_for_coming_back,
+                number_of_days_before_christmas,
             )
             <= 0
         ):
-            logger.log(f"Overdue for {reindeer_name} located {current_location}.")
+            logger.log(
+                f"Overdue for {reindeer.name} located {reindeer.current_location}."
+            )
             return True
         return False
 
