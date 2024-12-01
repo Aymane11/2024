1dca4fdd87f9ef3780602d5d7d9576403ea096a0
Aymane BOUMAAZA <aymane.boumaaza@innovx.ma>
2024-12-01 12:16:01 +0100

    Create reindeer class

---
+++ exercise/python/day01/communication/reindeer.py
@@ -0,0 +1,5 @@
+class Reindeer:
+    def __init__(self, name: str, current_location: str, number_of_days_for_coming_back: int):
+        self.name = name
+        self.current_location = current_location
+        self.number_of_days_for_coming_back = number_of_days_for_coming_back
